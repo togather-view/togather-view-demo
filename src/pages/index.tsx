fcdbc26aa6a2ac495a14421f2fd32ad55e37e9c2
Yejin Cho <orobos654@gmail.com>
2021-08-29 01:30:01 +0900

    Initial commit - Next.js Project Setting

---
+++ src/pages/index.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import styles from '@src/styles/Home.module.scss'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+<div>hi!</div></main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
