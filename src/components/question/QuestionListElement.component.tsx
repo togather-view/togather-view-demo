12495fd9de9b2d6dea00197271e7c6af21d2e45d
조예진 <40057032+ooooorobo@users.noreply.github.com>
2021-08-31 21:29:00 +0900

    [Feature] QuestionListElement에 질문 관련 정보 추가 (#4)

--- src/components/question/QuestionListElement.component.tsx
+++ src/components/question/QuestionListElement.component.tsx
@@ -1,5 +1,7 @@
-import { memo } from "react";
+import { memo, useMemo } from "react";
 import Link from "next/link";
+import { EyeOutlined, LikeOutlined, MessageOutlined } from "@ant-design/icons";
+import classNames from "classnames";
 
 import TagComponent from "@src/components/common/Tag.component";
 
@@ -12,24 +14,61 @@ interface Props {
 }
 
 function QuestionListElement({ question }: Props) {
+  const mobileIconClassName = useMemo(
+    () =>
+      classNames({
+        [styles.icon]: true,
+        [styles.mobile]: true,
+      }),
+    [],
+  );
+
+  const desktopIconClassName = useMemo(
+    () =>
+      classNames({
+        [styles.icon]: true,
+        [styles.desktop]: true,
+      }),
+    [],
+  );
+
   return (
     <div className={styles.listElementContainer}>
       <Link href={`/question/${question.id}`}>
-        <a>
+        <a className={styles.wrap}>
           {/* 직군 */}
-          <div className={styles.group}>
-            {question.jobGroup.map((x) => (
-              <span key={x.id}>{x.name}</span>
-            ))}
+          <div>
+            <div className={styles.group}>
+              {question.jobGroup.map((x) => (
+                <span key={x.id}>{x.name}</span>
+              ))}
+            </div>
+            <div />
+            {/*  질문 내용 */}
+            <h3 className={styles.title}>{question.contents}</h3>
+            {/*  관련 기술 */}
+            <div className={styles.techList}>
+              {question.techList.map((x) => (
+                <TagComponent key={x.id} title={x.name} color={x.color} />
+              ))}
+            </div>
           </div>
-          <div />
-          {/*  질문 내용 */}
-          <h3 className={styles.title}>{question.contents}</h3>
-          {/*  관련 기술 */}
-          <div className={styles.techList}>
-            {question.techList.map((x) => (
-              <TagComponent key={x.id} title={x.name} color={x.color} />
-            ))}
+          <div className={styles.info}>
+            <div className={styles.element}>
+              <LikeOutlined className={mobileIconClassName} />
+              <div className={styles.count}>0</div>
+              <LikeOutlined className={desktopIconClassName} />
+            </div>
+            <div className={styles.element}>
+              <MessageOutlined className={mobileIconClassName} />
+              <div className={styles.count}>2</div>
+              <MessageOutlined className={desktopIconClassName} />
+            </div>
+            <div className={styles.element}>
+              <EyeOutlined className={mobileIconClassName} />
+              <div className={styles.count}>23</div>
+              <EyeOutlined className={desktopIconClassName} />
+            </div>
           </div>
         </a>
       </Link>
