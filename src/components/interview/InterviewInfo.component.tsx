f94076fa561060f7fa6094a5fb2f6d502b494cb8
Yejin Cho <orobos654@gmail.com>
2021-10-10 14:52:09 +0900

    feat: Change info section design

--- src/components/interview/InterviewInfo.component.tsx
+++ src/components/interview/InterviewInfo.component.tsx
@@ -1,33 +1,58 @@
-import { memo } from "react";
+import { memo, useMemo } from "react";
+import { FiClock, FiSmile } from "react-icons/all";
 
 // interface
-import { JobGroup, Tech, User } from "@src/interface/interface";
+import { JobGroup, Tech } from "@src/interface/interface";
 
 // component
 import TagComponent from "@src/components/common/Tag.component";
 
 // styles
 import styles from "@src/styles/pages/InterviewMessenger.module.scss";
+import IconWithTooltipComponent from "@src/components/common/IconWithTooltip.component";
+import { Tooltip } from "antd";
 
 interface Props {
-  user: User;
   jobList: JobGroup[];
   techList: Tech[];
 }
 
-function InterviewInfoComponent({ user, jobList, techList }: Props) {
+function InterviewInfoComponent({ jobList, techList }: Props) {
+  const progressStyle = useMemo(() => ({ width: 100 }), []);
+
   return (
     <div className={styles.infoWrap}>
       <h1>투게더뷰</h1>
-      <h2>{user.firstName}님의 면접</h2>
+      {/* Progress bar */}
+      <div className={styles.progress}>
+        <div className={styles.background}>
+          <div className={styles.inner} style={progressStyle}>
+            <p>40%</p>
+          </div>
+        </div>
+        <p className={styles.description}>총 10개 질문 중 4개 진행</p>
+      </div>
+
+      <div className={styles.divider}>
+        <hr />
+      </div>
+      {/* Interview Details */}
+      <div className={styles.iconContainer}>
+        <Tooltip title="타이머 사용">
+          <FiClock className={styles.active} size={25} />
+        </Tooltip>
+        <Tooltip title="인성 질문 미포함">
+          <FiSmile className={styles.inactive} size={25} />
+        </Tooltip>
+      </div>
       <div className={styles.tagList}>
-        <h3>선택한 직무</h3>
+        <span className={styles.label}>직무</span>
         {jobList.map((x) => (
           <TagComponent title={x.name} key={x.id} />
         ))}
       </div>
       <div className={styles.tagList}>
-        <h3>선택한 기술</h3>
+        <span className={styles.label}>기술</span>
         {techList.map((x) => (
           <TagComponent title={x.name} color={x.color} key={x.id} />
         ))}
