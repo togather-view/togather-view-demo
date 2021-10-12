7a7d9d7475721cc8cd24b744a5996d752644d402
Yejin Cho <orobos654@gmail.com>
2021-10-12 20:51:55 +0900

    feat: Add scroll event in messenger body

--- src/components/interview/InterviewMessengerBody.component.tsx
+++ src/components/interview/InterviewMessengerBody.component.tsx
@@ -1,4 +1,12 @@
-import { memo, useContext, useMemo } from "react";
+import {
+  memo,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 
 // dummy
 import { questionListPage1 } from "@dummy/question.data";
@@ -15,6 +23,9 @@ import LoadingDotsComponent from "@src/components/common/LoadingDots.component";
 import styles from "@src/styles/pages/InterviewMessenger.module.scss";
 
 function InterviewMessengerBodyComponent() {
+  const bodyRef = useRef();
+  const [isScrolled, setScrolled] = useState(false);
+
   const { isMessageLeft, displayedList } = useContext(MessengerContext);
 
   const messageListDOM = useMemo(() => {
@@ -40,11 +51,32 @@ function InterviewMessengerBodyComponent() {
     return list;
   }, [displayedList, isMessageLeft]);
 
+  const trackScrolling = useCallback(() => {
+    const element = document.getElementById("messengerBody");
+    if (element.scrollTop < 0) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (typeof window !== undefined)
+      document
+        .getElementById("messengerBody")
+        .addEventListener("scroll", trackScrolling);
+  }, [trackScrolling]);
+
   return (
-    <div className={styles.messenger}>
+    <div id="messengerBody" className={styles.messenger} ref={bodyRef}>
       <div className={styles.messageWrap}>
         <div className={styles.message}>{messageListDOM}</div>
-        <AlertNewMessageComponent contents={questionListPage1[0].contents} />
+        {isScrolled && (
+          <AlertNewMessageComponent
+            bodyRef={bodyRef}
+            contents={questionListPage1[0].contents}
+          />
+        )}
       </div>
     </div>
   );
