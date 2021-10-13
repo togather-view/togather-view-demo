eaf21580216ac36b94abca391d8c82d677ddae07
Yejin Cho <orobos654@gmail.com>
2021-10-13 23:19:18 +0900

    feat: Separate result info section component

--- src/pages/interview/result/[interviewId]/index.tsx
+++ src/pages/interview/result/[interviewId]/index.tsx
@@ -1,36 +1,27 @@
 import { memo } from "react";
-import { CalendarTwoTone, QuestionCircleTwoTone } from "@ant-design/icons";
+
+// lib
+import { myAccount } from "@dummy/user.data";
 
 // components
 import LayoutComponent from "@src/components/common/Layout.component";
-import InterviewConditionIconContainerComponent from "@src/components/interview/InterviewConditionIconConatiner.component";
+import InterviewResultInfoComponent from "@src/components/interview/result/InterviewResultInfo.component";
 
 // styles
 import styles from "@src/styles/pages/InterviewResult.module.scss";
-import TagListComponent from "@src/components/common/TagList.component";
-import { myAccount } from "@dummy/user.data";
 
 function InterviewResultPage() {
   return (
     <LayoutComponent>
       <div className={styles.container}>
-        <div className={styles.info}>
-          <InterviewConditionIconContainerComponent useTimer containAttitude />
-          <div className={styles.wrapper}>
-            <div className={styles.detail}>
-              <CalendarTwoTone />
-              <p>2021년 10월 3일</p>
-            </div>
-            <div className={styles.detail}>
-              <QuestionCircleTwoTone />
-              <p>총 10가지 질문</p>
-            </div>
-          </div>
-          <div className={styles.wrapper}>
-            <TagListComponent label="직무" tagList={myAccount.jobList} />
-            <TagListComponent label="기술" tagList={myAccount.techList} />
-          </div>
-        </div>
+        <InterviewResultInfoComponent
+          useTimer={false}
+          containAttitude
+          interviewedAt="2021년 10월 3일"
+          totalQuestion={3}
+          jobList={myAccount.jobList}
+          techList={myAccount.techList}
+        />
         <div className={styles.list}>list</div>
       </div>
     </LayoutComponent>
