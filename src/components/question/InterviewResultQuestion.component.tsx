3f91313599c43b4d60f301e80e188c453ab3a0ec
Yejin Cho <orobos654@gmail.com>
2021-10-14 19:11:55 +0900

    feat: Separate InterviewResultQuestion component

---
+++ src/components/question/InterviewResultQuestion.component.tsx
@@ -0,0 +1,82 @@
+import { memo, useMemo } from "react";
+import Link from "next/link";
+import { Button, Switch } from "antd";
+
+// components
+import TagComponent from "@src/components/common/Tag.component";
+
+// styles
+import styles from "@src/styles/pages/InterviewResult.module.scss";
+
+function InterviewResultQuestionComponent({ questionToAnswer }) {
+  const techListDOM = useMemo(
+    () => (
+      <div className={styles.tagList}>
+        {questionToAnswer.question.techList.map((t) => (
+          <TagComponent key={t.id} title={t.name} color={t.color} />
+        ))}
+      </div>
+    ),
+    [questionToAnswer],
+  );
+
+  const jobListDOM = useMemo(
+    () => (
+      <div className={styles.tagList}>
+        {questionToAnswer.question.jobGroup.map((t) => (
+          <span key={t.id}>{t.name}</span>
+        ))}
+      </div>
+    ),
+    [questionToAnswer],
+  );
+
+  const buttonContainerDOM = useMemo(
+    () => (
+      <div className={styles.iconContainer}>
+        <Link href={`/question/${questionToAnswer.question.id}`}>
+          <a>
+            <Button
+              icon={<span className={styles.icon}>👀</span>}
+              shape="circle"
+            />
+          </a>
+        </Link>
+        <Button icon={<span className={styles.icon}>👍</span>} shape="circle" />
+        <Button icon={<span className={styles.icon}>🔖</span>} shape="circle" />
+        <Button icon={<span className={styles.icon}>🚨</span>} shape="circle" />
+      </div>
+    ),
+    [questionToAnswer.question.id],
+  );
+
+  return (
+    <div key={questionToAnswer.id}>
+      {/* 질문 */}
+      <div>
+        {/* 질문 헤더 */}
+        <div className={styles.header}>
+          <div className={styles.tagContainer}>
+            {techListDOM}
+            {jobListDOM}
+          </div>
+          {buttonContainerDOM}
+        </div>
+        {/* 질문 내용 */}
+        <div className={styles.contents}>
+          <h3>{questionToAnswer.question.contents}</h3>
+        </div>
+      </div>
+      {/* 답변 내용 */}
+      <div className={styles.answer}>{questionToAnswer.answer.contents}</div>
+      {/* 답변 조작 */}
+      <div className={styles.switch}>
+        <p>답변을 다른 사람에게 공개할까요?</p>
+        <Switch size="small" />
+      </div>
+      <hr />
+    </div>
+  );
+}
+
+export default memo(InterviewResultQuestionComponent);
--- src/pages/interview/result/[interviewId]/index.tsx
+++ src/pages/interview/result/[interviewId]/index.tsx
@@ -7,12 +7,10 @@ import { questionToAnswer } from "@dummy/interview.data";
 // components
 import LayoutComponent from "@src/components/common/Layout.component";
 import InterviewResultInfoComponent from "@src/components/interview/result/InterviewResultInfo.component";
+import InterviewResultQuestionComponent from "@src/components/question/InterviewResultQuestion.component";
 
 // styles
 import styles from "@src/styles/pages/InterviewResult.module.scss";
-import TagComponent from "@src/components/common/Tag.component";
-import { Button, Switch } from "antd";
-import { DownCircleTwoTone } from "@ant-design/icons";
 
 function InterviewResultPage() {
   return (
@@ -28,57 +26,7 @@ function InterviewResultPage() {
         />
         <div className={styles.list}>
           {questionToAnswer.map((x) => (
-            <div key={x.id}>
-              <div>
-                <div>
-                  <div className={styles.header}>
-                    <div className={styles.tagContainer}>
-                      <div className={styles.tagList}>
-                        {x.question.techList.map((t) => (
-                          <TagComponent
-                            key={t.id}
-                            title={t.name}
-                            color={t.color}
-                          />
-                        ))}
-                      </div>
-                      <div className={styles.jobList}>
-                        {x.question.jobGroup.map((t) => (
-                          <span key={t.id}>{t.name}</span>
-                        ))}
-                      </div>
-                    </div>
-                    <div className={styles.iconContainer}>
-                      <Button
-                        icon={<span className={styles.icon}>👀</span>}
-                        shape="circle"
-                      />
-                      <Button
-                        icon={<span className={styles.icon}>👍</span>}
-                        shape="circle"
-                      />
-                      <Button
-                        icon={<span className={styles.icon}>🔖</span>}
-                        shape="circle"
-                      />
-                      <Button
-                        icon={<span className={styles.icon}>🚨</span>}
-                        shape="circle"
-                      />
-                    </div>
-                  </div>
-                  <div className={styles.contents}>
-                    <h3>{x.question.contents}</h3>
-                  </div>
-                </div>
-              </div>
-              <div className={styles.answer}>{x.answer.contents}</div>
-              <div className={styles.switch}>
-                <p>답변을 다른 사람에게 공개할까요?</p>
-                <Switch size="small" />
-              </div>
-              <hr />
-            </div>
+            <InterviewResultQuestionComponent key={x.id} questionToAnswer={x} />
           ))}
         </div>
       </div>
