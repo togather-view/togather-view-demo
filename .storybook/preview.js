32a6d91c909be06426bc39ebb7ad68a96f3b86e5
Yejin Cho <orobos654@gmail.com>
2021-10-15 15:02:49 +0900

    feat: Add viewports

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -9,6 +9,30 @@ export const parameters = {
 addParameters({
   viewport: {
     viewports: {
+      mobile1: {
+        name: "Small mobile",
+        styles: {
+          height: "568px",
+          width: "320px",
+        },
+        type: "mobile",
+      },
+      mobile2: {
+        name: "Large mobile",
+        styles: {
+          height: "896px",
+          width: "414px",
+        },
+        type: "mobile",
+      },
+      tablet: {
+        name: "Tablet",
+        styles: {
+          height: "1112px",
+          width: "834px",
+        },
+        type: "tablet",
+      },
       desktop: {
         name: "desktop",
         styles: {
